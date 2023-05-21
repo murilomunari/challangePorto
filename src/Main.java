0d78dc82f1bc98323978492df2340a69e16a22a5
murilomunari <munarimurilo441@gmail.com>
2023-05-21 15:29:01 -0300

    sprint2

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,37 @@
 import br.com.challange.model.*;
-
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
 
         Bicicleta bicicleta;
-        Aro aro;
-        Material material;
-        Pneus pneus;
-        Quadro quadro;
-        Marcha marcha;
-        Suspensao suspensao;
-        Guidao guidao;
         int encerrar = 0;
-
 
         do {
             var nomeBicicleta = JOptionPane.showInputDialog("Bicicleta");
-            bicicleta = new Bicicleta(nomeBicicleta);
-
-            var tipoMaterial = JOptionPane.showInputDialog("material");
-
+            var tipoMaterial = JOptionPane.showInputDialog("Material");
             var tipoPneus = JOptionPane.showInputDialog("Pneus");
-
             var tipoAro = JOptionPane.showInputDialog("Aro");
-
             var tipoQuadro = JOptionPane.showInputDialog("Quadro");
-
             var tipoMarcha = JOptionPane.showInputDialog("Marcha");
-
             var tipoSuspensao = JOptionPane.showInputDialog("Suspensão");
-
             var tipoGuidao = JOptionPane.showInputDialog("Guidão");
 
-            bicicleta = new Bicicleta(tipoMaterial, tipoGuidao, tipoAro, tipoPneus, tipoQuadro, tipoMarcha,tipoSuspensao);
+            bicicleta = new Bicicleta(tipoMaterial, tipoGuidao, tipoAro, tipoPneus, tipoQuadro, tipoMarcha, tipoSuspensao);
 
             var pergunta = """
-                    
-                    Deseja Cadastar mais uma bicicleta?
-                    
+                    Deseja cadastrar mais uma bicicleta?
                     [ 1 ] SIM
-                    
                     [ 0 ] NÃO
-
                     """;
             encerrar = Integer.parseInt(JOptionPane.showInputDialog(pergunta));
 
+        } while (encerrar > 0);
 
-        }while (encerrar >0);
 
+        JOptionPane.showMessageDialog(null, "Bicicleta cadastrada: " + bicicleta);
+
+
+        System.exit(0);
     }
 }
